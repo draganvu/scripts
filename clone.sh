a697eb5c30bbf778787135cef1f99889e1dec6f6
draganvu <dvujovic@protonmail.ch>
2024-10-10 00:55:48 +0100

    Moved scripts from dotfiles

---
+++ clone.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+#
+# Author: Dragan Vujović <dvujovic@protonmail.ch>
+# License: GNU GPL v3+
+# URL: https://github.com/draganvu/dotfiles
+#
+# Commentary:
+# Script for setting up my github projects
+# Requires `git' package installed
+
+echo "Preparing to clone..."
+
+# Set variable
+GIT_FOLDER="$HOME/github"
+
+# Create directory if it doesn't exist
+echo "Creating directory..."
+mkdir -p "$GIT_FOLDER"
+
+cd "$GIT_FOLDER"
+
+# Function to clone repostories
+clone_repo() {
+    REPO_URL=$1
+    REPO_NAME=$(basename "$REPO_URL" .git)
+
+    if [ -d "$REPO_NAME" ]; then
+	echo "$REPO_NAME already exists. Skipping..."
+    else
+	echo "Cloning $REPO_NAME..."
+	git clone "$REPO_URL"
+    fi
+}
+
+# Clone repositories
+clone_repo https://github.com/draganvu/dotfiles
+clone_repo https://github.com/draganvu/emacs-config
+clone_repo https://github.com/draganvu/draganvu.github.io
+clone_repo https://github.com/draganvu/cv-latex
+
+echo "Clone complete!..."
+
+#EOF
---
+++ post-install.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+#
+# Author: Dragan Vujović <dvujovic@protonmail.ch>
+# License: GNU GPL v3+
+# URL: https://github.com/draganvu/dotfiles
+#
+# Commentary:
+# A script to install software after Ubuntu installation
+#
+
+# Update the system
+echo "Updating package list..."
+sudo apt update
+
+# Upgrade existing packages
+echo "Upgrading existing packages..."
+sudo apt upgrade -y
+
+# Install Git (version control)
+echo "Installing git..."
+sudo apt install -y git
+
+# Install Emacs text editor
+echo "Installing Emacs..."
+sudo apt install -y emacs
+
+# Install graphics/image editing software
+echo "Installing Inkscape..."
+sudo apt install -y inkscape
+echo "Installing GIMP..."
+sudo apt install -y gimp
+
+# Install Terminal (Urxvt)
+echo "Installing Terminal..."
+sudo apt install -y rxvt-unicode
+
+# Install download tools
+echo "Installing curl, wget..."
+sudo apt install -y curl wget
+
+# Install Solitaire card game
+sudo apt install -y aisleriot
+
+# Cleanup
+sudo apt autoremove -y
+
+# Finish message
+echo "Sofware installation completed!"
